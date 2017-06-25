2dce2aa4830b913fdc3de6531a26b73d7df24bb7
mekhti11 <mekhti11@gmail.com>
2017-06-25 05:05:25 +0300

    read data from localhost

--- public/skateStoreController.js
+++ public/skateStoreController.js
@@ -1,17 +1,13 @@
 angular.module("skatestore")
-.controller("skateStoreCtrl", function ($scope) {
+.constant("dataUrl","http://localhost:2403/products")
+.controller("skateStoreCtrl", function ($scope,$http,dataUrl) {
 
-  $scope.data = {
-    products:[
-    {"name":"Noire Bad Ass Deck","description":"Tüm Noire tahtalar, 100% Kanada Akçaağaçı üretimi olup, Jessup marka zımpara hediyelidir.","category":"Kaykay Tahtalari","price":152,"id":"d9c5016b81637893"},
-    {"name":"Noire Splash Deck","description":"Tüm Noire tahtalar, 100% Kanada Akçaağaçı üretimi olup, Jessup marka zımpara hediyelidir.","category":"Kaykay Tahtalari","price":160,"id":"e9e91cebea67386d"},
-    {"name":"Reflex 5.25 Flat Black Truck","description":"Reflex 2 adet profesyonel kaykay truckı.","category":"Truck/Dingiller","price":90,"id":"1996e308b181b93f"},
-    {"name":"Slant 5,25 Black Gold Truck","description":"Slant fiyat kalite performansı son derece yüksek bir trucktır. Sağlam ve dengelidir. Fiyat iki adet yani bir set truck için geçerlidir. Uzunluk: 5,25 Yükseklik: Regular","category":"Truck/Dingiller","price":135,"id":"55934336773a5827"},
-    {"name":"BILLABONG Elevation Erkek T-shirt","description":"BILLABONG, özgürlük ve özgünlüğü yaşam tarzı haline getiren, heyecan sporlarının lider lifestyle markalarındandır.","category":"T-Shirt","price":41.99,"id":"6510b4bebd3ad8c8"},{"name":"Sector 9 ABEC 5 BEARING","description":"Sector 9, longboard dünyasının en başarılı markalarının başında yer almakta. Hem yeni, hem teknik tüm longboard kitlesine hitap eden Sector9 birbirinden harika tasarımları, güncel modelleri, longboard truck, longboard slide eldiveni vb. tüm longboard aksesuarları/parçaları ile en fiyat en çok seçenek ile Türkiye’nin kaykay sitesi SBT’de!","category":"Rullman","price":49,"id":"8ffb0457897d781f"},
-    {"name":"Andale Abec 5 Rulman","description":"Andale kaykay dünyasının en tepesinde yer alan kaykaycılardan olan Joey Brezinski ve P.Rod tarafından kurulmuştur. Bu kadar güçlü iki isim yan yana geldiğinde ise dünyanın en epik kaykay takımını kurabilecek bir karizma oluşmuştur. Andale takımı Daewon Song, Ryan Sheckler, Guy Mariano, Lucas Puig gibi onlarca inanılmaz ismi bir araya getirmiştir.","category":"Rullman","price":60,"id":"d713bb72a38afa00"},
-    {"name":"Blind Avenger Black Complete 7.6","description":"Tahta: Blind kaykay tahtaları dünyanın bir numaralı kaykay malzemesi olan Kanada hardrock akçaağacından üretilir. Her tahta tek bir kalıpta basılır, buda tahtanın sahip olduğu eğimleri mükemmelleştirir. Ayrıca Blind tahtlarında Resin adlı yapıştırcı kullanılır. Bu yapıştırıcı bilinen en kaliteli yapıştırıcıdır. Bunun sayesinde Blind kaykay tahtaları çok daha sağlam olurken normal bir tahtadan daha hafifi bir hale gelmektedir.Tahta Genişliği: 7,6Truck: Blind Trucks Gloss Black. Tekerlek: 53mm Blind Reaper Black Metal Tekerlekler.","category":"Pro Model Complete Kaykaylar", "price":370, "id":"1999853ee7042a71"},
-    {"name":"QUIKSILVER ANASKULL COMPLETE 8″","description":"Heyecan sporlarının lideri Quiksilver, tüm yeni kaykaya başlayanlar için yüksek kaliteli harika bir koleksiyon yarattı. İster caddede, ister parkta ve ülkemizde henüz olmasa bile tüm POOL’larda keyifle kayabilirsiniz.  Uzunluk: 32 inç Genişlik: 8 inç Truck: 5,25 inç Tekerler: 52 mm Konkeyv: Medium Özellikler Quiksilver Logo Tasarımı En iyi kalite 52mm tekerler 7-ply Kanada Akçaağaç Siyah Zımpara","category":"Pro Model Complete Kaykaylar","price":429.99,"id":"534887e0ad23d8cb"}
-    ]
+  $scope.data = { };
 
-  };
+  $http.get(dataUrl)
+    .then(function(response){
+      $scope.data.products = response.data;
+    },function(error){
+      $scope.data.products = error;
+    });
 });
